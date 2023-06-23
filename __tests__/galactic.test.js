b7c0adbbd7e4d54f43395b84b69b1a2b72237f20
Jason E. Church <elijahchurch@gmail.com>
2023-06-23 08:48:24 -0700

    create and run first test for Galaxycalc constructor

--- __tests__/galactic.test.js
+++ __tests__/galactic.test.js
@@ -1,16 +1,23 @@
-// import Rectangle from '../src/js/rectangle.js';
-
-// describe('Rectangle', () => {
-//     let rectangle;
-
-//     beforeEach(() => {
-//         rectangle = new Rectangle(3, 5);
-//     });
-
-//     test('should correctly create a rectangle object using two sides', () => {
-//         expect(rectangle.side1).toEqual(3);
-//         expect(rectangle.side2).toEqual(5);
-//     });
-// // 
-
 import Galaxycalc from "../src/js/galactic";
+
+describe("Galaxycalc object and its methods", () => {
+    let newUser;
+
+    beforeEach(() => {
+        newUser = new Galaxycalc(32);
+    });
+
+    test("it should create a calculator object that contains the user current age", () => {
+        expect(this.earthAge).toEqual(32);
+        expect(this.mercuryAge).toEqual(0);
+        expect(this.venusAge).toEqual(0);
+        expect(this.marsAge).toEqual(0);
+        expect(this.jupiterAge).toEqual(0);
+        expect(this.earthPassage).toEqual(0);
+        expect(this.mercuryPassage).toEqual(0);
+        expect(this.venusPassage).toEqual(0);
+        expect(this.marsPassage).toEqual(0);
+        expect(this.jupeiterPassage).toEqual(0);
+    });
+
+})
