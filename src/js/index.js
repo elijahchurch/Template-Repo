7b62d53245cbb355ee42ef1b4314509d1ea53106
Jason E. Church <elijahchurch@gmail.com>
2023-06-23 15:33:56 -0700

    update html and index.js to correctly handle the main form event

--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "../css/styles.css";
 import Galaxycalc from './galactic';
 
+function handleForm(event) {
+    event.preventDefault();
+    const userAge = document.getElementById("age").value;
+    let newUser = new Galaxycalc(userAge);
+    newUser.setGalacticAges();
+    document.getElementById("earth-age").innerText = `You are ${newUser.earthAge} Earth years old!`;
+    document.getElementById("mercury-age").innerText = `You are ${newUser.mercuryAge} Mercury years old!`;
+    document.getElementById("venus-age").innerText = `You are ${newUser.venusAge} Venus years old!`;
+    document.getElementById("mars-age").innerText = `You are ${newUser.marsAge} Mars years old!`;
+    document.getElementById("jupiter-age").innerText = `You are ${newUser.jupiterAge} Jupiter years old!`;
+}
+
+const form = document.querySelector("form");
+form.addEventListener("submit", handleForm);
