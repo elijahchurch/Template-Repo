44f8811331b753f71ad2412de75aa9faabc3eeb5
Jason E. Church <elijahchurch@gmail.com>
2023-06-23 09:52:02 -0700

    update constructor to pass updated test

--- src/js/galactic.js
+++ src/js/galactic.js
@@ -5,11 +5,11 @@ export default class Galaxycalc {
         this.venusAge = 0;
         this.marsAge = 0;
         this.jupiterAge = 0;
-        this.earthPassage = 0;
-        this.mercuryPassage = 0;
-        this.venusPassage = 0;
-        this.marsPassage = 0;
-        this.jupiterPassage = 0;
+        this.earthPassage = "";
+        this.mercuryPassage = "";
+        this.venusPassage = "";
+        this.marsPassage = "";
+        this.jupiterPassage = "";
     }
 
     setGalacticAges() {
