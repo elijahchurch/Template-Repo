6be55a537346f343846eac993f16b71c946308e8
Jason E. Church <elijahchurch@gmail.com>
2023-06-23 08:44:56 -0700

    update title names in the package.json and webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
             verbose: true
         }),
         new HtmlWebpackPlugin({
-            title: 'Shape Tracker',
+            title: 'Galactic Calculator',
             template: './src/index.html',
             inject: 'body'
         })
