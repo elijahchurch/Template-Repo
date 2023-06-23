2a103d0d269f7344ff8a26fdeb7ee61108788eea
Jason E. Church <elijahchurch@gmail.com>
2023-06-23 16:18:39 -0700

    update handleForm function to clear input value

--- src/js/index.js
+++ src/js/index.js
@@ -5,14 +5,15 @@ import Galaxycalc from './galactic';
 
 function handleForm(event) {
     event.preventDefault();
-    const userAge = document.getElementById("age").value;
-    let newUser = new Galaxycalc(userAge);
+    const userAge = document.getElementById("age");
+    let newUser = new Galaxycalc(userAge.value);
     newUser.setGalacticAges();
     document.getElementById("earth-age").innerText = `You are ${newUser.earthAge} Earth years old!`;
     document.getElementById("mercury-age").innerText = `You are ${newUser.mercuryAge} Mercury years old!`;
     document.getElementById("venus-age").innerText = `You are ${newUser.venusAge} Venus years old!`;
     document.getElementById("mars-age").innerText = `You are ${newUser.marsAge} Mars years old!`;
     document.getElementById("jupiter-age").innerText = `You are ${newUser.jupiterAge} Jupiter years old!`;
+    userAge.value = "";
 }
 
 const form = document.querySelector("form");
